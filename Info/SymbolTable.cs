0c091760b831ff8e5fddffbb718c960a96041af7
dreamanlan <dreaman_163@163.com>
2018-11-12 17:33:26 +0800

    lua关键字修改

--- Info/SymbolTable.cs
+++ Info/SymbolTable.cs
@@ -634,7 +634,7 @@ namespace RoslynTool.CsToDsl
         private static string s_SystemDllPath = string.Empty;
 
         private static HashSet<string> s_ExtraDslKeywords = new HashSet<string> {
-            "and", "elseif", "end", "function", "local", "null", "not", "or", "repeat", "then", "until"
+            "and", "elseif", "end", "function", "local", "nil", "not", "or", "repeat", "then", "until"
         };
         private static HashSet<string> s_BasicTypes = new HashSet<string> {
             "System.Boolean", "System.Byte", "System.SByte", "System.Int16", "System.UInt16", "System.Int32", "System.UInt32", "System.Int64", "System.UInt64", "System.Single", "System.Double"
