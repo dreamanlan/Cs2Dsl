b6150d64c94a9b7b2c3dab5bf422a7a5b7a7554e
dreamanlan <dreaman_163@163.com>
2018-12-05 18:09:35 +0800

    外部容器与对象创建时不提供构造函数名字（外部对象函数重载不换名，提供名字没必要）

--- CsToDsl/CsToDsl.cs
+++ CsToDsl/CsToDsl.cs
@@ -477,19 +477,19 @@ namespace RoslynTool.CsToDsl
                             bool isList = IsImplementationOfSys(namedTypeSym, "IList");
                             if (isDictionary) {
                                 //字典对象的处理
-                                CodeBuilder.AppendFormat("new{0}dictionary({1}", isExternal ? "extern" : string.Empty, fullTypeName);
+                                CodeBuilder.AppendFormat("new{0}dictionary({1}, ", isExternal ? "extern" : string.Empty, fullTypeName);
                                 CsDslTranslater.OutputTypeArgsInfo(CodeBuilder, namedTypeSym);
                             } else if (isList) {
                                 //列表对象的处理
-                                CodeBuilder.AppendFormat("new{0}list({1}", isExternal ? "extern" : string.Empty, fullTypeName);
+                                CodeBuilder.AppendFormat("new{0}list({1}, ", isExternal ? "extern" : string.Empty, fullTypeName);
                                 CsDslTranslater.OutputTypeArgsInfo(CodeBuilder, namedTypeSym);
                             } else {
                                 //集合对象的处理
-                                CodeBuilder.AppendFormat("new{0}collection({1}", isExternal ? "extern" : string.Empty, fullTypeName);
+                                CodeBuilder.AppendFormat("new{0}collection({1}, ", isExternal ? "extern" : string.Empty, fullTypeName);
                                 CsDslTranslater.OutputTypeArgsInfo(CodeBuilder, namedTypeSym);
                             }
                         } else {
-                            CodeBuilder.AppendFormat("new{0}object({1}", isExternal ? "extern" : string.Empty, fullTypeName);
+                            CodeBuilder.AppendFormat("new{0}object({1}, ", isExternal ? "extern" : string.Empty, fullTypeName);
                             CsDslTranslater.OutputTypeArgsInfo(CodeBuilder, namedTypeSym);
                         }
                         if (!isExternal) {
