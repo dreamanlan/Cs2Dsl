df747cc02506a1100f9afd134f6ae6d13735e652
dreamanlan <dreaman_163@163.com>
2020-08-28 23:16:45 +0800

    去掉对lua操作符重载的支持，直接调c#方法

--- copy.bat
+++ copy.bat
@@ -1,3 +1,5 @@
+..\Hostx64\x64\editbin.exe /stack:10240000 bin\Debug\Cs2Dsl.exe
+
 copy /y bin\Debug\Cs2Dsl.exe c:\Code\Client\Tools\cs2dsl\Cs2Dsl.exe
 copy /y bin\Debug\Cs2Dsl.exe d:\Code\Client\Tools\cs2dsl\Cs2Dsl.exe
 
