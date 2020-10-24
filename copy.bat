36f44dbd359fc66b1540537f551febac22029080
dreamanlan <dreaman_163@163.com>
2020-10-24 11:10:16 +0800

    外部引用类型精确确定，输出名按dotnet reflection api的名称格式输出，方便用于导出白名单

--- copy.bat
+++ copy.bat
@@ -1,4 +1,4 @@
-..\Hostx64\x64\editbin.exe /stack:10240000 bin\Debug\Cs2Dsl.exe
+..\Hostx64\x64\editbin.exe /stack:40960000 bin\Debug\Cs2Dsl.exe
 
 copy /y bin\Debug\Cs2Dsl.exe c:\Code\Client\Tools\cs2dsl\Cs2Dsl.exe
 copy /y bin\Debug\Cs2Dsl.exe d:\Code\Client\Tools\cs2dsl\Cs2Dsl.exe
