8a8cb0d0d78fde405a27d470118add8c072faf5b
dreamanlan <dreaman_163@163.com>
2021-03-03 03:52:33 +0800

    添加xlua的coroutine机制

--- copy.bat
+++ copy.bat
@@ -2,5 +2,6 @@
 
 copy /y bin\Debug\Cs2Dsl.exe c:\Code\Client\Tools\cs2dsl\Cs2Dsl.exe
 copy /y bin\Debug\Cs2Dsl.exe d:\Code\Client\Tools\cs2dsl\Cs2Dsl.exe
+copy /y bin\Debug\Cs2Dsl.exe e:\Code\Client\Tools\cs2dsl\Cs2Dsl.exe
 
 @pause
